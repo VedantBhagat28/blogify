a14c3307f07f4e185df60927fed4785777d40863
VedantBhagat28 <vedantbhagat2004@gmail.com>
2024-07-02 18:19:56 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,52 @@
+const { Router } = require("express");
+const User = require("../models/user");
+
+const router = Router();
+
+router.get("/signin", (req, res) => {
+  return res.render("signin");
+});
+
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const token = await User.matchPasswordAndGenerateToken(email, password);
+
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    return res.render("signin", {
+      error: "Incorrect Email or Password",
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("token").redirect("/");
+});
+
+router.post("/signup", async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+
+    await User.create({
+      fullName,
+      email,
+      password,
+    });
+
+    return res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    return res.redirect("/user/signup");
+  }
+});
+
+module.exports = router;
